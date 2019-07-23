cfd3464bbe2a2206f95d24187fb4216707a5cda8
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-07-23 14:07:42 -0400

    LS8 will take edge inputs into account.

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -7,7 +7,12 @@ from cpu import *
 
 cpu = CPU()
 program = "default"
-if 2 <= len(sys.argv):
+if len(sys.argv) == 2:
     program = sys.argv[1]
+elif len(sys.argv) > 2:
+    print("Only one program can be ran at a time. First argument will be used.")
+    program = sys.argv[1]
+elif len(sys.argv) < 2:
+    print("No program provided. Default program will be ran.")
 cpu.load(program)
 cpu.run()
